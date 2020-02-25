13d75a849053140895d7827351d820cd1b75f2fe
Enterman <450746539@qq.com>
2020-02-25 18:06:17 +0800

    INITIAL_COMMIT

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,17 +1,19 @@
-import java.util.Arrays;
-
 import cf.kody.main.Main;
 
+import java.util.Arrays;
+
 public class Start {
-	
 
-    public static void main(String[] args) {
-        Main.main(concat(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
-    }
+	// 用GBK的是傻逼。如果你用GBK我杀了你的妈
+	// 额 你能看到说明你使用了UTF-8，感谢你。
 
-    public static <T> T[] concat(T[] first, T[] second) {
-        T[] result = Arrays.copyOf(first, first.length + second.length);
-        System.arraycopy(second, 0, result, first.length, second.length);
-        return result;
-    }
+	public static void main(String[] args) {
+		Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
+	}
+
+	public static <T> T[] concat(T[] first, T[] second) {
+		T[] result = Arrays.copyOf(first, first.length + second.length);
+		System.arraycopy(second, 0, result, first.length, second.length);
+		return result;
+	}
 }
