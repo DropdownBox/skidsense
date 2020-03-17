6925824d5c3f27cefd27d80a4c9ec579ee24645e
Enterman <450746539@qq.com>
2020-03-17 16:22:56 +0800

    Update NoRotate.java

--- src/main/java/me/skidsense/module/collection/world/NoRotate.java
+++ src/main/java/me/skidsense/module/collection/world/NoRotate.java
@@ -4,6 +4,7 @@
 package me.skidsense.module.collection.world;
 
 import me.skidsense.hooks.Sub;
+import me.skidsense.hooks.events.EventPacketRecieve;
 import me.skidsense.hooks.events.EventPacketSend;
 import me.skidsense.module.Mod;
 import me.skidsense.module.ModuleType;
@@ -12,18 +13,16 @@ import java.awt.Color;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 
 public class NoRotate
-extends Mod {
-    public NoRotate() {
-        super("No Rotate", new String[]{"rotate"}, ModuleType.World);
-        this.setColor(new Color(17, 250, 154).getRGB());
-    }
-    @Sub
-    private void onPacket(EventPacketSend e) {
-        if (e.getPacket() instanceof S08PacketPlayerPosLook) {
-            S08PacketPlayerPosLook look = (S08PacketPlayerPosLook)e.getPacket();
-            look.yaw = this.mc.thePlayer.rotationYaw;
-            look.pitch = this.mc.thePlayer.rotationPitch;
-        }
-    }
+		extends Mod {
+	public NoRotate() {
+		super("No Rotate", new String[]{"rotate"}, ModuleType.World);
+		this.setColor(new Color(17, 250, 154).getRGB());
+	}
+	@Sub
+	private void onPacket(EventPacketRecieve e) {
+		if (e.getPacket() instanceof S08PacketPlayerPosLook) {
+			e.setCancelled(true);
+		}
+	}
 }
 
