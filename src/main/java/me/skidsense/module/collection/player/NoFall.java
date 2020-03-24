c8ac54b1de18610cc2f5736d58e1eb14dfe32da2
supreb <215789586@qq.com>
2020-03-24 10:10:31 +0800

    更新NoFall

--- src/main/java/me/skidsense/module/collection/player/NoFall.java
+++ src/main/java/me/skidsense/module/collection/player/NoFall.java
@@ -4,13 +4,14 @@ import me.skidsense.hooks.Sub;
 import me.skidsense.hooks.events.EventPreUpdate;
 import me.skidsense.module.Mod;
 import me.skidsense.module.ModuleType;
-import me.skidsense.util.MoveUtil;
+import net.minecraft.block.BlockAir;
 import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.util.BlockPos;
 
 import java.awt.*;
 
 public class NoFall extends Mod {
-    double fall;
+    private int state;
 
 
     public NoFall() {
@@ -19,20 +20,48 @@ public class NoFall extends Mod {
     }
 
     @Override
-    public void onEnable(){
-        fall = 0;
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
     }
 
     @Sub
     private void onUpdate(EventPreUpdate e) {
-        if (!MoveUtil.isOnGround(0.001)) {
-            if (mc.thePlayer.motionY < -0.08)
-                fall -= mc.thePlayer.motionY;
-            if (fall > 2) {
-                fall = 0;
-                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
-            }
-        } else
-            fall = 0;
+        if (mc.thePlayer.fallDistance > 3.0f && this.isBlockUnder()) {
+            mc.getNetHandler().addToSendQueue(new C03PacketPlayer(true));
+            state = 2;
+        } else if (state == 2 && mc.thePlayer.fallDistance < 2) {
+            mc.thePlayer.motionY = 0.001D;
+            state = 3;
+            return;
+        }
+        switch (state) {
+            case 3:
+                mc.thePlayer.motionY = 0.001D;
+                state = 4;
+                break;
+            case 4:
+                mc.thePlayer.motionY = 0.001D;
+                state = 5;
+                break;
+            case 5:
+                mc.thePlayer.motionY = 0.001D;
+                state = 1;
+                break;
+        }
+    }
+
+    private boolean isBlockUnder() {
+        for (int i = (int) mc.thePlayer.posY; i > 0; --i) {
+            BlockPos pos = new BlockPos(mc.thePlayer.posX, (double) i, mc.thePlayer.posZ);
+            if (this.mc.theWorld.getBlockState(pos).getBlock() instanceof BlockAir)
+                continue;
+            return true;
+        }
+        return false;
     }
 }
