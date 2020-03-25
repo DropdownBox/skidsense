f9bb3a71a9476a5b59fa1a582d51678b64aafac6
supreb <215789586@qq.com>
2020-03-25 20:26:47 +0800

    NoFall

--- src/main/java/me/skidsense/module/collection/player/NoFall.java
+++ src/main/java/me/skidsense/module/collection/player/NoFall.java
@@ -1,18 +1,14 @@
 package me.skidsense.module.collection.player;
 
 import me.skidsense.hooks.Sub;
-import me.skidsense.hooks.events.EventPacketRecieve;
+import me.skidsense.hooks.events.EventPacketSend;
 import me.skidsense.module.Mod;
 import me.skidsense.module.ModuleType;
-import net.minecraft.block.BlockAir;
 import net.minecraft.network.play.client.C03PacketPlayer;
-import net.minecraft.util.BlockPos;
 
 import java.awt.*;
 
 public class NoFall extends Mod {
-
-
     public NoFall() {
         super("No Fall", new String[]{"Nofalldamage", "nofall"}, ModuleType.Player);
         setColor(new Color(242, 137, 73).getRGB());
@@ -29,20 +25,10 @@ public class NoFall extends Mod {
     }
 
     @Sub
-    private void onUpdate(EventPacketRecieve e) {
-            if (mc.thePlayer.fallDistance > 3.0 && e.getPacket() instanceof C03PacketPlayer && isBlockUnder()) {
-                C03PacketPlayer Packet = (C03PacketPlayer) e.getPacket();
-                Packet.onGround = (mc.thePlayer.fallDistance) % 3 == 0;
+    private void onUpdate(EventPacketSend e) {
+        if (mc.thePlayer.fallDistance > 3.0f) {
+            C03PacketPlayer Packet = (C03PacketPlayer) e.getPacket();
+            Packet.onGround =(mc.thePlayer.fallDistance) % 3 == 0;
         }
-    }
-
-    private boolean isBlockUnder() {
-        for (int i = (int) mc.thePlayer.posY; i > 0; --i) {
-            BlockPos pos = new BlockPos(mc.thePlayer.posX, (double) i, mc.thePlayer.posZ);
-            if (this.mc.theWorld.getBlockState(pos).getBlock() instanceof BlockAir)
-                continue;
-            return true;
-        }
-        return false;
     }
 }
