cfff1f6668e82775bf6c0e3ebc05ccb205cdcd60
白露型驅逐艦-夕立 <winxpqq955@163.com>
2020-08-25 14:44:43 +0800

    新的ItemESP

--- src/main/java/me/skidsense/module/collection/visual/ItemEsp.java
+++ src/main/java/me/skidsense/module/collection/visual/ItemEsp.java
@@ -1,5 +1,6 @@
 package me.skidsense.module.collection.visual;
 
+import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
 
 import me.skidsense.hooks.Sub;
@@ -14,6 +15,8 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.util.AxisAlignedBB;
 
+import java.awt.*;
+
 public class ItemEsp extends Mod {
 	
 	public Option outlinedboundingBox = new Option("Outlined", "Outlined", true);
@@ -25,31 +28,52 @@ public class ItemEsp extends Mod {
 	
 	@Sub
 	public void onRender(EventRender3D event) {
+		GL11.glPushMatrix();
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glDepthMask(false);
+		GL11.glLineWidth(1.0F);
+
+		GL11.glTranslated(-mc.getRenderManager().viewerPosX, -mc.getRenderManager().viewerPosY, -mc.getRenderManager().viewerPosZ);
+
 		for (Object o : mc.theWorld.loadedEntityList) {
-    		if (!(o instanceof EntityItem)) continue;
-    		EntityItem item = (EntityItem)o;
- 		   	double itemposX = item.posX;
- 		   	double x = itemposX - Minecraft.getMinecraft().getRenderManager().renderPosX;
- 		   itemposX = item.posY + 0.5D;
- 		   	double y = itemposX - mc.getRenderManager().renderPosY;
- 		   itemposX = item.posZ;
- 		   	double z = itemposX - mc.getRenderManager().renderPosZ;
- 		   	GL11.glEnable(3042);
- 		   	GL11.glLineWidth(2.0F);
- 		   	GL11.glColor4f(1, 1, 1, .75F);
- 		   	GL11.glDisable(3553);
- 		   	GL11.glDisable(2929);
- 		   	GL11.glDepthMask(false);
-            if((Boolean) this.outlinedboundingBox.getValue()) {
- 	   			RenderUtil.drawOutlinedBoundingBox(new AxisAlignedBB(x - .2D, y-0.05, z - .2D, x + .2D, y - 0.45d, z + .2D));
- 	   		}else {
- 	   			GL11.glColor4f(1, 1, 1, 0.15f);
- 	 	   		RenderUtil.drawBoundingBox(new AxisAlignedBB(x - .2D, y-0.05, z - .2D, x + .2D, y - 0.45d, z + .2D));
- 	   		}
- 	   		GL11.glEnable(3553);
- 	   		GL11.glEnable(2929);
- 	   		GL11.glDepthMask(true);
- 	   		GL11.glDisable(3042);
-    	}
+			if (o instanceof EntityItem) {
+				EntityItem entityItem = (EntityItem) o;
+
+				double posX = entityItem.lastTickPosX + (entityItem.posX - entityItem.lastTickPosX) * event.getPartialTicks();
+				double posY = entityItem.lastTickPosY + (entityItem.posY - entityItem.lastTickPosY) * event.getPartialTicks();
+				double posZ = entityItem.lastTickPosZ + (entityItem.posZ - entityItem.lastTickPosZ) * event.getPartialTicks();
+
+				GL11.glColor4d(1, 1, 1, 0.2);
+
+				if (entityItem.getEntityItem() != null) {
+					ItemStack stack = entityItem.getEntityItem();
+					Color color = new Color(stack.getItem().getColorFromItemStack(stack, 1));
+
+					GL11.glColor4d(color.getRed(), color.getGreen(), color.getBlue(), 0.2);
+				}
+
+				AxisAlignedBB boundingBox = new AxisAlignedBB(posX - 0.25, posY, posZ - 0.25,
+						posX + 0.25, posY + 0.5, posZ + 0.25);
+
+				RenderUtil.drawBoundingBox(boundingBox);
+				RenderUtil.drawOutlinedBoundingBox(boundingBox);
+			}
+		}
+
+		GL11.glTranslated(mc.getRenderManager().viewerPosX, mc.getRenderManager().viewerPosY, mc.getRenderManager().viewerPosZ);
+
+		GL11.glDepthMask(true);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+		GL11.glPopMatrix();
 	}
 }
