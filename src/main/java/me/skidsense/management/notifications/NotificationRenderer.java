cd941297c11c22f1f9f561548ae8d7354dcac5a2
白露型驅逐艦-夕立 <winxpqq955@163.com>
2020-09-10 22:09:25 +0800

    新的fontrenderer

--- src/main/java/me/skidsense/management/notifications/NotificationRenderer.java
+++ src/main/java/me/skidsense/management/notifications/NotificationRenderer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.lwjgl.opengl.GL11;
 
+import me.skidsense.Client;
 import me.skidsense.color.Colors;
 import net.minecraft.client.Minecraft;
 
@@ -21,9 +22,9 @@ public class NotificationRenderer implements INotificationRenderer {
          notification.translate.interpolate(60.0F, (float)y, 0.5F);
          GL11.glPushMatrix();
          GL11.glScaled(0.9f, 0.9f, 0.9f);
-         Minecraft.getMinecraft().fontRendererObj.drawStringWithShadow(notification.getMessage(), 80.0F, notification.translate.getY(), Colors.getColor(255, (int)notification.opacity.getOpacity()));
+         //Minecraft.getMinecraft().fontRendererObj.drawStringWithShadow(notification.getMessage(), 80.0F, notification.translate.getY(), Colors.getColor(255, (int)notification.opacity.getOpacity()));
          GL11.glPopMatrix();
-         //Client.fontManager.verdana16.drawStringWithShadow(notification.getMessage(), 60.0F, notification.translate.getY(), Colors.getColor(255, (int)notification.opacity.getOpacity()));
+         Client.fontManager.comfortaa16.drawStringWithShadow(notification.getMessage(), 60.0F, notification.translate.getY(), Colors.getColor(255, (int)notification.opacity.getOpacity()));
          y += 10;
          if (notification.checkTime() >= notification.getDisplayTime() + notification.getInitializeTime()) {
             notification.targetOpacity = 0;
