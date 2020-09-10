851453ab4e20b69904f3526cdb8fe68c1723069b
白露型驅逐艦-夕立 <winxpqq955@163.com>
2020-09-10 22:08:38 +0800

    新的GuiMainMenu

---
+++ src/main/java/me/skidsense/util/Panorama.java
@@ -0,0 +1,220 @@
+package me.skidsense.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.Project;
+
+/**
+ * @author antja03
+ */
+public class Panorama {
+
+    private final GuiScreen screen;
+    private final ResourceLocation[] paths;
+
+    private DynamicTexture viewportTexture;
+    private ResourceLocation backgroundTexture;
+
+    private int panoramaTimer;
+
+    public Panorama(GuiScreen screen, ResourceLocation path0, ResourceLocation path1, ResourceLocation path2, ResourceLocation path3, ResourceLocation path4, ResourceLocation path5) {
+        this.screen = screen;
+        this.paths = new ResourceLocation[]{path0, path1, path2, path3, path4, path5};
+    }
+
+    public Panorama(GuiScreen screen, Panorama panorama) {
+        this.screen = screen;
+        this.paths = new ResourceLocation[]{panorama.getPaths()[0], panorama.getPaths()[1], panorama.getPaths()[2], panorama.getPaths()[3], panorama.getPaths()[4], panorama.getPaths()[5]};
+        this.panoramaTimer = panorama.getPanoramaTimer();
+    }
+
+    public void init() {
+        this.viewportTexture = new DynamicTexture(256, 256);
+        this.backgroundTexture = Minecraft.getMinecraft().getTextureManager().getDynamicTextureLocation("background", this.viewportTexture);
+    }
+
+    public void update() {
+        panoramaTimer++;
+    }
+
+    public void draw(int mouseX, int mouseY, float partialTicks) {
+        GlStateManager.disableAlpha();
+        this.renderSkybox(mouseX, mouseY, partialTicks);
+        GlStateManager.enableAlpha();
+    }
+
+    private void drawPanorama(int p_73970_1_, int p_73970_2_, float p_73970_3_) {
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.matrixMode(5889);
+        GlStateManager.pushMatrix();
+        GlStateManager.loadIdentity();
+        Project.gluPerspective(120.0F, 1.0F, 0.05F, 10.0F);
+        GlStateManager.matrixMode(5888);
+        GlStateManager.pushMatrix();
+        GlStateManager.loadIdentity();
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.rotate(90.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.disableCull();
+        GlStateManager.depthMask(false);
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        int i = 8;
+
+        for (int j = 0; j < i * i; ++j) {
+            GlStateManager.pushMatrix();
+            float f = ((float) (j % i) / (float) i - 0.5F) / 64.0F;
+            float f1 = ((float) (j / i) / (float) i - 0.5F) / 64.0F;
+            float f2 = 0.0F;
+            GlStateManager.translate(f, f1, f2);
+            GlStateManager.rotate(MathHelper.sin(((float) this.panoramaTimer + p_73970_3_) / 400.0F) * 25.0F + 20.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.rotate(-((float) this.panoramaTimer + p_73970_3_) * 0.1F, 0.0F, 1.0F, 0.0F);
+
+            for (int k = 0; k < 6; ++k) {
+                GlStateManager.pushMatrix();
+
+                if (k == 1) {
+                    GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
+                }
+
+                if (k == 2) {
+                    GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+                }
+
+                if (k == 3) {
+                    GlStateManager.rotate(-90.0F, 0.0F, 1.0F, 0.0F);
+                }
+
+                if (k == 4) {
+                    GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+                }
+
+                if (k == 5) {
+                    GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+                }
+
+                Minecraft.getMinecraft().getTextureManager().bindTexture(paths[k]);
+                worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+                int l = 255 / (j + 1);
+                float f3 = 0.0F;
+                worldrenderer.pos(-1.0D, -1.0D, 1.0D).tex(0.0D, 0.0D).color(255, 255, 255, l).endVertex();
+                worldrenderer.pos(1.0D, -1.0D, 1.0D).tex(1.0D, 0.0D).color(255, 255, 255, l).endVertex();
+                worldrenderer.pos(1.0D, 1.0D, 1.0D).tex(1.0D, 1.0D).color(255, 255, 255, l).endVertex();
+                worldrenderer.pos(-1.0D, 1.0D, 1.0D).tex(0.0D, 1.0D).color(255, 255, 255, l).endVertex();
+                tessellator.draw();
+                GlStateManager.popMatrix();
+            }
+
+            GlStateManager.popMatrix();
+            GlStateManager.colorMask(true, true, true, false);
+        }
+
+        worldrenderer.setTranslation(0.0D, 0.0D, 0.0D);
+        GlStateManager.colorMask(true, true, true, true);
+        GlStateManager.matrixMode(5889);
+        GlStateManager.popMatrix();
+        GlStateManager.matrixMode(5888);
+        GlStateManager.popMatrix();
+        GlStateManager.depthMask(true);
+        GlStateManager.enableCull();
+        GlStateManager.enableDepth();
+    }
+
+    /**
+     * Rotate and blurs the skybox view in the main menu
+     */
+    private void rotateAndBlurSkybox(float p_73968_1_) {
+        Minecraft.getMinecraft().getTextureManager().bindTexture(this.backgroundTexture);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GL11.glCopyTexSubImage2D(GL11.GL_TEXTURE_2D, 0, 0, 0, 0, 0, 256, 256);
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        GlStateManager.colorMask(true, true, true, false);
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+        GlStateManager.disableAlpha();
+        int i = 3;
+
+        for (int j = 0; j < i; ++j) {
+            float f = 1.0F / (float) (j + 1);
+            int k = screen.width;
+            int l = screen.height;
+            float f1 = (float) (j - i / 2) / 256.0F;
+            worldrenderer.pos((double) k, (double) l, (double) screen.zLevel).tex((double) (0.0F + f1), 1.0D).color(1.0F, 1.0F, 1.0F, f).endVertex();
+            worldrenderer.pos((double) k, 0.0D, (double) screen.zLevel).tex((double) (1.0F + f1), 1.0D).color(1.0F, 1.0F, 1.0F, f).endVertex();
+            worldrenderer.pos(0.0D, 0.0D, (double) screen.zLevel).tex((double) (1.0F + f1), 0.0D).color(1.0F, 1.0F, 1.0F, f).endVertex();
+            worldrenderer.pos(0.0D, (double) l, (double) screen.zLevel).tex((double) (0.0F + f1), 0.0D).color(1.0F, 1.0F, 1.0F, f).endVertex();
+        }
+
+        tessellator.draw();
+        GlStateManager.enableAlpha();
+        GlStateManager.colorMask(true, true, true, true);
+    }
+
+    /**
+     * Renders the skybox in the main menu
+     */
+    private void renderSkybox(int p_73971_1_, int p_73971_2_, float p_73971_3_) {
+        Minecraft.getMinecraft().getFramebuffer().unbindFramebuffer();
+        GlStateManager.viewport(0, 0, 256, 256);
+        this.drawPanorama(p_73971_1_, p_73971_2_, p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        this.rotateAndBlurSkybox(p_73971_3_);
+        Minecraft.getMinecraft().getFramebuffer().bindFramebuffer(true);
+        GlStateManager.viewport(0, 0, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+        float f = screen.width > screen.height ? 120.0F / (float) screen.width : 120.0F / (float) screen.height;
+        float f1 = (float) screen.height * f / 256.0F;
+        float f2 = (float) screen.width * f / 256.0F;
+        int i = screen.width;
+        int j = screen.height;
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+        worldrenderer.pos(0.0D, (double) j, (double) screen.zLevel).tex((double) (0.5F - f1), (double) (0.5F + f2)).color(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
+        worldrenderer.pos((double) i, (double) j, (double) screen.zLevel).tex((double) (0.5F - f1), (double) (0.5F - f2)).color(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
+        worldrenderer.pos((double) i, 0.0D, (double) screen.zLevel).tex((double) (0.5F + f1), (double) (0.5F - f2)).color(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
+        worldrenderer.pos(0.0D, 0.0D, (double) screen.zLevel).tex((double) (0.5F + f1), (double) (0.5F + f2)).color(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
+        tessellator.draw();
+    }
+
+    public GuiScreen getScreen() {
+        return screen;
+    }
+
+    public ResourceLocation[] getPaths() {
+        return paths;
+    }
+
+    public DynamicTexture getViewportTexture() {
+        return viewportTexture;
+    }
+
+    public ResourceLocation getBackgroundTexture() {
+        return backgroundTexture;
+    }
+
+    public int getPanoramaTimer() {
+        return panoramaTimer;
+    }
+
+    public void setPanoramaTimer(int panoramaTimer) {
+        this.panoramaTimer = panoramaTimer;
+    }
+}
