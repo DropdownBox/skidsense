caf3353586295b9cc65d15e931796bb356a0ea9c
白露型驅逐艦-夕立 <winxpqq955@163.com>
2020-09-10 22:15:49 +0800

    夜视修复

--- src/main/java/me/skidsense/module/collection/visual/Fullbright.java
+++ src/main/java/me/skidsense/module/collection/visual/Fullbright.java
@@ -4,24 +4,21 @@ import me.skidsense.hooks.Sub;
 import me.skidsense.hooks.events.EventPreUpdate;
 import me.skidsense.module.Mod;
 import me.skidsense.module.ModuleType;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class Fullbright extends Mod {
-	public int alphag = 1;
 	public Fullbright() {
 		super("Full Bright", new String[]{"FullBright"}, ModuleType.Visual);
-        //this.addValues();
 	}
 
 	@Override
 	public void onDisable() {
-		mc.gameSettings.gammaSetting = (float) 1;
+		mc.thePlayer.removePotionEffect(Potion.nightVision.getId());
 	}
 
 	@Sub
 	public void onUpdate(EventPreUpdate event) {
-		if(alphag < 15 && alphag > 0) {
-			alphag++;
-		}
-		mc.gameSettings.gammaSetting = alphag;
+		mc.thePlayer.addPotionEffect(new PotionEffect(Potion.nightVision.getId(), 1337, 1));
 	}
 }
