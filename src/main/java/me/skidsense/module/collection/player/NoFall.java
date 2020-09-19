39a0ff793882d021c775b8e96f03cf932cab85cf
supreb <215789586@qq.com>
2020-09-19 11:00:45 +0800

    使得nofall完全无伤

--- src/main/java/me/skidsense/module/collection/player/NoFall.java
+++ src/main/java/me/skidsense/module/collection/player/NoFall.java
@@ -2,24 +2,35 @@ package me.skidsense.module.collection.player;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import me.skidsense.Client;
 import me.skidsense.hooks.Sub;
+import me.skidsense.hooks.events.EventPacketSend;
 import me.skidsense.hooks.events.EventPreUpdate;
 import me.skidsense.module.Mod;
 import me.skidsense.module.ModuleType;
+import me.skidsense.module.collection.move.Flight;
+import me.skidsense.util.MoveUtil;
 import net.minecraft.network.play.client.C03PacketPlayer;
 
 import java.awt.*;
 
 public class NoFall extends Mod {
+    double fall;
+
     public NoFall() {
-        super("No Fall", new String[]{"Nofalldamage","NoFall"}, ModuleType.Player);
+        super("No Fall", new String[]{"Nofalldamage", "NoFall"}, ModuleType.Player);
         setColor(new Color(242, 137, 73).getRGB());
     }
 
     @Sub
-    private void onUpdate(EventPreUpdate e) {
-        if (mc.thePlayer.fallDistance > 3.0F) {
-            mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(mc.thePlayer.ticksExisted % ThreadLocalRandom.current().nextInt(45, 75) != 0));
+    private void onPacketSend(EventPacketSend e){
+        if(e.getPacket() instanceof C03PacketPlayer && mc.thePlayer.fallDistance > 2.0F && !Client.getModuleManager().getModuleByClass(Flight.class).isEnabled()){
+            C03PacketPlayer packetPlayer = (C03PacketPlayer)e.getPacket();
+            if (!packetPlayer.isMoving()) {
+                packetPlayer.onGround = true;
+            } else {
+                mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C03PacketPlayer(true));
+            }
         }
     }
 }
